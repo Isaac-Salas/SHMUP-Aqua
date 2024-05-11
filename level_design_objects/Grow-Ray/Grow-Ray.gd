e9ef120de23e25c120178215c7875fbe95c43a5f
Isaac Salas <78274906+Isaac-Salas@users.noreply.github.com>
2024-05-11 12:36:35 -0600

    Initial commit

---
+++ level_design_objects/Grow-Ray/Grow-Ray.gd
@@ -0,0 +1,18 @@
+extends Node3D
+@export var target_size = 2.5
+signal shrink_player(target_size)
+
+@onready var shrink_ray = $RayCast3D
+
+func _process(_delta):
+	if shrink_ray.is_colliding():
+		var collider = shrink_ray.get_collider()
+	if shrink_ray.is_colliding():
+		var collider = shrink_ray.get_collider()
+		match collider.name:
+			'Player':
+				$"../Player"._on_ray_collided(target_size)
+				GlobalVar.MinCap = target_size
+				GlobalVar.MaxCap = target_size
+				GlobalVar.sizefactor = target_size
+			
