7ca41453780dd9bc1e53095ce5ae0a968cd68b95
Isaac Salas <iscprepa@gmail.com>
2024-05-11 23:16:38 -0600

    Added more animations and better animation system
    Yes

--- player/player.gd
+++ player/player.gd
@@ -5,24 +5,42 @@ extends RigidBody2D
 @onready var scale_component = $ScaleComponent
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var move_component = $MoveComponent
+var change = "Center"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	fire_rate_timer.timeout.connect(shoot)
+	#fire_rate_timer.timeout.connect(shoot)
 	pass # Replace with function body.
 
+func _input(event):
+	print(change)
+	if Input.is_action_just_pressed("ui_shoot"):
+		animated_sprite_2d.flip_h = false
+		change = "Shoot"
+		shoot()
+	elif Input.is_action_pressed("ui_left"):
+		animated_sprite_2d.flip_h = true
+		change = "Right_Left"
+	elif Input.is_action_pressed("ui_right"):
+		animated_sprite_2d.flip_h = false
+		change = "Right_Left"
+	elif Input.is_action_pressed("ui_up"):
+		change = "Up_Down"
+		animated_sprite_2d.flip_v = false
+	elif Input.is_action_pressed("ui_down"):
+		change = "Up_Down"
+		animated_sprite_2d.flip_v = true
+	else:
+		change = "Center"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#print(move_component.velocity)
 	animation()
 
 func animation():
-	if move_component.velocity.y<0:
-		animated_sprite_2d.play("Up")
-	elif move_component.velocity.y>0:
-		animated_sprite_2d.play("Down")
-	else:
-		animated_sprite_2d.play("Center")
+	animated_sprite_2d.play(change)
+
 
 func shoot():
 	spawner_component.spawn(centershot.global_position)
