d92726107b168ecbc43dd35abead367a7eed34ba
Isaac Salas <iscprepa@gmail.com>
2024-05-12 22:22:35 -0600

    Stuff to fix tomorrow

--- scenes/intro.gd
+++ scenes/intro.gd
@@ -1,9 +1,21 @@
 extends Node2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var animation_player = $AnimationPlayer
-
+@onready var canstart = false
+@onready var timer = $Timer
 
 
 
 func _on_timer_timeout():
 	animation_player.play("AppearText")
+	timer.autostart = false
+	canstart = true
+
+func _input(event):
+	if Input.is_anything_pressed():
+		match canstart:
+			false:
+				pass
+			true:
+				animation_player.play("RESET")
+			
